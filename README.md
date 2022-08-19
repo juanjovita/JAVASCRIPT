3eed7741b5be2bb989536706fbbd8f779b898bc3
juanjovita <juanjovita22@gmail.com>
2022-08-19 16:26:37 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-gx8lkz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-gx8lkz)
---
+++ script.js
@@ -0,0 +1,35 @@
+
+    console.log('Hello!');
+    // alert('Olá, viadinho')
+    document.getElementById('nome').value ='oi'
+    
+    
+    /* 
+    *Variáveis*
+    -----DECLARAÇÕES-----------------------------------------
+    Não podem conter números, apenas com letras ou *-*
+    Não podem conter caracteres espeiciais
+    Não podem ser iguais ou palavras reservadas da linguagem
+    
+    +CASE SENSITIVE+
+    as variáveis nome Nome e NOME, são variáveis diferentes
+    ---------------------------------------------------------
+     */
+    
+    //STRING
+    var texto = 'Curso de Javascript'
+    
+    //NUMBER
+    var numeroInteiro = -7
+    var numeroFracionado = 11210.75
+    
+    //BOOLEAN
+    var teste = true
+    
+    
+    // alert(numeroFracionado) //abrir um dialog
+    document.write(numeroFracionado)
+    
+    //ver  no console
+    console.log(numeroInteiro)
+        
